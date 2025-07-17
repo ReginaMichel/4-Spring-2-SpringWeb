038c7e778f73d370b326cd5f46b312431077d669
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-07-17 14:32:37 +0200

    added method which greets a person by name via PathVariable.

--- src/main/java/org/example/spring2springweb/controller/HelloController.java
+++ src/main/java/org/example/spring2springweb/controller/HelloController.java
@@ -1,6 +1,7 @@
 package org.example.spring2springweb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class HelloController {
     @GetMapping
     public String sayHelloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/{id}")
+    public String sayHello(@PathVariable String id) {
+        return "Hallo, " + id + "!";
     }
 }
