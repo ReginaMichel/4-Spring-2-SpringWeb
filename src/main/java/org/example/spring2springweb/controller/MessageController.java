418f8921f71032f9fc94d6e03c5d88f4d07b25b9
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-07-17 15:29:12 +0200

    added DeleteMapping to remove certain messages via PathVariable. Changed field variable of MessageController from List<Message> to Map<String, Message>.

--- src/main/java/org/example/spring2springweb/controller/MessageController.java
+++ src/main/java/org/example/spring2springweb/controller/MessageController.java
@@ -3,22 +3,27 @@ package org.example.spring2springweb.controller;
 import org.example.spring2springweb.model.Message;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
 
-    private final List<Message> messages = new ArrayList<>();
+    private final Map<String, Message> messages = new HashMap<>();
 
     @GetMapping
-    public List<Message> getMessages() {
+    public Map<String, Message> getMessages() {
         return messages;
     }
 
     @PostMapping
     public void postMessage(@RequestBody Message message) {
-        messages.add(message);
+        messages.put(message.id(), message);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMessage(@PathVariable String id) {
+        messages.remove(id);
     }
 }
